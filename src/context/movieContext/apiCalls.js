a06cac5c986498bc0eba41d54a177c0088b9e0b7
Aritra <chatterjee.aritra@protonmail.com>
2023-07-18 13:39:37 +0530

    movies context

---
+++ src/context/movieContext/apiCalls.js
@@ -0,0 +1,13 @@
+import { getMoviesStart, getMoviesFailure, getMoviesSuccess } from "./MovieActions"
+
+export const getMovies = async (dispatch) =>{
+  dispatch(getMoviesStart());
+  try {
+    const res = await axios.get("/movies", {
+      headers: { token: "Bearer" + JSON.parse(localStorage.getItem("user")).accessToken},
+    });
+    dispatch(getMoviesSuccess(res.data));
+  } catch (err) {
+    dispatch(getMoviesFailure());
+  }
+}
