4809c32fd05f37fc8ad0a40650da33d19b7594db
Aritra <chatterjee.aritra@protonmail.com>
2023-07-26 01:19:50 +0530

    firebase

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import "./newProduct.css"
+import { storage } from '../../firebase';
+
+
 
 function NewProduct() {
   const [movie, setMovie] = useState(null);
@@ -8,13 +11,45 @@ function NewProduct() {
   const [imgSm, setImgSm] = useState(null);
   const [trailer, setTrailer] = useState(null);
   const [video, setVideo] = useState(null);
+  const [uploaded, setUploaded] = useState(0);  
+
 
   const handleChange = (e)=>{
     const value = e.target.value;
     setMovie({...movie, [e.target.name]:value})
   };
+  
+  const upload = (items) =>{
+    items.forEach(item=>{
+      const uploadTask = storage.ref(`/items/${item.file.name}`).put(item.file);
+      uploadTask.on("state_changes",(snapshot)=>{
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + " % done");
+      },
+        (err) => {console.log(err)},()=>{
+          uploadTask.snapshot.ref.getDownloadURL().then(url=>{
+            setMovie((prev) => { 
+              return {...prev, [item.label]:url};
+            });
+            setUploaded((prev) => prev+1);
+          });
+        }
+      );
+    });
+  };
 
-  console.log(movie)
+  const handleUpload = (e) =>{
+    e.preventDefault();
+    upload([
+      {file: img, label: "img"},
+      {file: imgTitle, label: "imgTitle"},
+      {file: imgSm, label: "imgSm"},
+      {file: trailer, label: "trailer"},
+      {file: video, label: "video"},
+    ]);
+  };
+  
+  console.log(movie);
 
   return (
     <div className='newProduct'>
@@ -70,8 +105,11 @@ function NewProduct() {
         <div className="addProductItem">
           <label>Video</label>
           <input type="file" name='video' onChange={e=>setVideo(e.target.files[0])}/>
-        </div>
+        </div>{uploaded === 5 ?(
         <button className="addProductButton">Create</button>
+        ) : (
+        <button className="addProductButton" onClick={handleUpload}>Upload</button>
+        )}
       </form>
     </div>
   )
